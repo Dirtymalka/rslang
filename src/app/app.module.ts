f1e5246d05e7873bd10891300f85654d3a184a8a
Maksim Tihomirov <maxbeere@yandex.ru>
2021-03-23 21:11:57 +0300

    feat: add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { HeaderModule } from './modules/header/header.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { StoreModule } from '@ngrx/store';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
