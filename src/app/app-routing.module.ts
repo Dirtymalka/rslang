7ffa5fbd8f42db99eb561b459772d5dd987873b8
Eugenia Shibkova <shibkova91@gmail.com>
2021-03-24 22:38:43 +0300

    feat: add textbook route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { GamesComponent } from './modules/games';
+import { TextbookComponent } from './modules/textbook/components/textbook/textbook.component';
 
 const routes: Routes = [
   {
     path: 'games',
     component: GamesComponent,
+  },
+  {
+    path: 'textbook',
+    component: TextbookComponent,
   },
 ];
 
