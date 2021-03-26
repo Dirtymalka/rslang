0bb18cd9bf26c90b73856b18c38875447cb1ff7f
Eugenia Shibkova <shibkova91@gmail.com>
2021-03-26 20:52:39 +0300

    feat: add words-list modddule

---
+++ src/app/modules/words-list/components/sound-icon/icon-sound.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-sound',
+  templateUrl: './icon-sound.component.html',
+  styleUrls: ['./icon-sound.component.scss'],
+})
+export class IconSoundComponent {}
