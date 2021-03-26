05992379f9a26734af03df2af368252e4434d37b
Eugenia Shibkova <shibkova91@gmail.com>
2021-03-26 20:57:51 +0300

    refactor: modify textbook module

--- src/app/modules/textbook/textbook.module.ts
+++ src/app/modules/textbook/textbook.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { TextbookComponent } from './components/textbook/textbook.component';
 import { ChaptersComponent } from './components/chapters/chapters.component';
 import { ChaptersItemComponent } from './components/chapters-item/chapters-item.component';
 import { WordsListModule } from '../words-list/words-list.module';
 import { TextbookRoutingModule } from './textbook-routing.module';
+import { ControlBarModule } from '../control-bar/components/control-bar.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [TextbookComponent, ChaptersComponent, ChaptersItemComponent],
-  imports: [CommonModule, WordsListModule, TextbookRoutingModule],
-  exports: [TextbookComponent, ChaptersComponent],
+  imports: [
+    SharedModule,
+    WordsListModule,
+    TextbookRoutingModule,
+    ControlBarModule,
+  ],
+  exports: [TextbookComponent],
 })
 export class TextbookModule {}
