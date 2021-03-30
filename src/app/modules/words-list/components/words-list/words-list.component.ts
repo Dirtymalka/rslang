2c06631246e9d386016fd6bfc2f20da9d850035e
Eugenia Shibkova <shibkova91@gmail.com>
2021-03-30 22:29:02 +0300

    refactor: refactor textbook elements styles and view

--- src/app/modules/words-list/components/words-list/words-list.component.ts
+++ src/app/modules/words-list/components/words-list/words-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { WordsServiceService } from '../../../shared/services/words-service.service';
 
 @Component({
   selector: 'app-words-list',
   templateUrl: './words-list.component.html',
   styleUrls: ['./words-list.component.scss'],
 })
-export class WordsListComponent {}
+export class WordsListComponent {
+  constructor(private wordsService: WordsServiceService) {}
+}
