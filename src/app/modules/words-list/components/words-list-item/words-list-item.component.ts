068fcb0c9ef0fc8d30d1e3ab712cf7d8cf7c51c9
Eugenia Shibkova <shibkova91@gmail.com>
2021-03-31 21:45:20 +0300

    refactor: refactor words-list module

--- src/app/modules/words-list/components/words-list-item/words-list-item.component.ts
+++ src/app/modules/words-list/components/words-list-item/words-list-item.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {
+  selectIsShowWordTranslation,
+  selectIsShowDifficultWordButton,
+  selectIsShowDeleteWordButton,
+} from '../../../../redux/selectors/settings.selectors';
+import { IAppState } from '../../../../redux/state/app.state';
+import { IWord } from '../../../../redux/models/word.models';
 
 @Component({
   selector: 'app-words-list-item',
@@ -6,5 +15,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./words-list-item.component.scss'],
 })
 export class WordsListItemComponent {
-  isWordView: true;
+  @Input()
+  word: IWord;
+
+  isShowWordTranslation$: Observable<boolean> = this.store$.select(
+    selectIsShowWordTranslation,
+  );
+
+  isShowDifficultWordButton$: Observable<boolean> = this.store$.select(
+    selectIsShowDifficultWordButton,
+  );
+
+  isShowDeleteWordButton$: Observable<boolean> = this.store$.select(
+    selectIsShowDeleteWordButton,
+  );
+
+  constructor(private store$: Store<IAppState>) {}
+
+  onDiffucultButtonClick(): void {
+    console.log('difficult');
+    console.log(this.word);
+  }
+
+  onDeleteButtonClick(): void {
+    console.log('delete');
+    console.log(this.word);
+  }
 }
--- src/app/modules/words-list/components/words-list/words-list.component.ts
+++ src/app/modules/words-list/components/words-list/words-list.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { IAppState } from '../../../../redux/state/app.state';
+import { selectGroup } from '../../../../redux/selectors/settings.selectors';
+import { IWord } from '../../../../redux/models/word.models';
 import { WordsServiceService } from '../../../shared/services/words-service.service';
+import { fetchAllWordsSuccess } from '../../../../redux/actions/words.actions';
 
 @Component({
   selector: 'app-words-list',
@@ -7,5 +13,43 @@ import { WordsServiceService } from '../../../shared/services/words-service.serv
   styleUrls: ['./words-list.component.scss'],
 })
 export class WordsListComponent {
-  constructor(private wordsService: WordsServiceService) {}
+  listWords: IWord[] = [];
+
+  group;
+
+  group$: Subscription = this.store$
+    .select(selectGroup)
+    .subscribe((group: number) => {
+      this.group = group;
+
+      this.getWordsList();
+    });
+
+  constructor(
+    private wordsService: WordsServiceService,
+    private store$: Store<IAppState>,
+  ) {}
+
+  getWordsList(): void {
+    this.wordsService.getWords(this.group, 0).subscribe(
+      (listWords: IWord[]) => {
+        console.log(listWords);
+        this.listWords = listWords;
+        console.log('list', this.listWords);
+
+        this.store$.dispatch(fetchAllWordsSuccess({ words: this.listWords }));
+      },
+      (error) => {
+        console.log(error.message);
+      },
+    );
+  }
+
+  markAsDifficult(word: IWord): void {
+    console.log('list', word);
+  }
+
+  markAsDelete(word: IWord): void {
+    console.log('list', word);
+  }
 }
