ab9c0cbfddaaee33e9e00a1a813a63a729ea8771
YuriySga <ess1928@yandex.ru>
2021-04-06 18:54:42 +0400

    implement-audio-challenge

--- src/app/redux/reducers/games.reducer.ts
+++ src/app/redux/reducers/games.reducer.ts
@@ -2,7 +2,10 @@ import { createReducer, on } from '@ngrx/store';
 import { fetchACallWordsWithLevelsSuccess } from '../actions/audioCall.actions';
 import { hangmanReducer } from './games.hangman.reducer';
 import { fetchWordsWithLevelsSuccess } from '../actions/hangman.actions';
+<<<<<<< HEAD
 import { userLogout } from '../actions/user.actions';
+=======
+>>>>>>> 59ea3c2 (implement-audio-challenge)
 import { audioCallReducer } from './games.audioCall.reducer';
 
 const initialGamesState = {
